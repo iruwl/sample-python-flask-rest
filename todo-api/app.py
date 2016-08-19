3e3c90ccdae328a656470dae27450ed31fe5d24b
iruwl <irul_sylva@yahoo.com>
2016-08-19 22:46:44 +0700

    add auth

--- todo-api/app.py
+++ todo-api/app.py
@@ -1,8 +1,10 @@
 #!flask/bin/python
 #http://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask
 from flask import Flask, jsonify, abort, make_response
+from flask.ext.httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 
 #ignore slash after
 app.url_map.strict_slashes = False
@@ -22,6 +24,16 @@ tasks = [
     }
 ]
 
+@auth.get_password
+def get_password(username):
+    if username == 'irul':
+        return 'python'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
@@ -31,11 +43,13 @@ def index():
     return "Hello, World!"
 
 @app.route('/tasks', methods=['GET']) #, strict_slashes=False)
+@auth.login_required
 def get_tasks():
     return jsonify({'tasks': tasks})
 
 #@app.route('/tasks', methods=['GET']) #, strict_slashes=False
 @app.route('/tasks/<int:task_id>', methods=['GET'])
+@auth.login_required
 def get_task(task_id=None):
     #if task_id == None:
     #    return jsonify({'tasks': tasks})
